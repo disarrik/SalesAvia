0493758933e6d098cc8b7ab62f8f3b109e35857e
disar <disarra2002@gmail.com>
2022-04-03 15:29:02 +0300

    1)Добавил Dockerfile для создания контейнера с neo4j
    2)Добавил файлы для инициализации данных, файлы добавляются в контейнер через volume, перерд запуском сервера neo4j запускается скрипт миграции данных
    
    3)Добавил контейнер neo4j в docker-compose.yml

---
+++ router-service/src/main/java/ru/disarra/salesavia/routerservice/RouterServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.disarra.salesavia.routerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouterServiceApplication.class, args);
+    }
+
+}
---
+++ router-service/src/test/java/ru/disarra/salesavia/routerservice/RouterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.disarra.salesavia.routerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
