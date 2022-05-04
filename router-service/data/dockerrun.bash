19aa3154f48823c7c813c42c0120d3e345030ffe
disar <disarra2002@gmail.com>
2022-05-04 21:56:41 +0300

    1)Убрал свой образ neo4j 2)Настроил миграции для профиля dev

--- router-service/data/dockerrun.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d -p 7474:7474 -p 7687:7687 -v "$(pwd)/volumes/import:/var/lib/neo4j/import" -v "$(pwd)/volumes/data:/data" -v "$(pwd)/volumes/logs:/logs" -v "$(pwd)/volumes/plugins:/plugins" -e NEO4J_AUTH=none -e NEO4JLABS_PLUGINS=\[\"apoc\"\] --name my_neo4j $1
--- router-service/data/volumes/import/migration.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/var/lib/neo4j/bin/neo4j-admin import --database=neo4j --force --nodes="/var/lib/neo4j/import/airports.csv" --relationships="/var/lib/neo4j/import/travels.csv" --trim-strings=true
