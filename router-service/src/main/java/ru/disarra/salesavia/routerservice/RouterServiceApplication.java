c3c397571c175b7caed461f33b2ad801757f1e41
disar <disarra2002@gmail.com>
2022-05-04 17:29:50 +0300

    Интеграция с Eureka

--- router-service/src/main/java/ru/disarra/salesavia/routerservice/RouterServiceApplication.java
+++ router-service/src/main/java/ru/disarra/salesavia/routerservice/RouterServiceApplication.java
@@ -2,8 +2,10 @@ package ru.disarra.salesavia.routerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RouterServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RouterServiceApplication.class, args);
